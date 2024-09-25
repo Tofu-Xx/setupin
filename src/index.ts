ab579d0e7bba55a3fa707afe8e6c504e3637321b
Tofu-Xx <1714843907@qq.com>
2024-09-26 02:38:55 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,20 @@ import {
 import { Vue } from "./assets/vue.js";
 
 window["Vue"] = Vue;
-Object.entries(Vue).forEach(([k, v]) => window[k] = v);
-const portal = proxyCall([
-  "onMounted",
-  "onUpdated",
-  "onUnmounted",
-  "onBeforeMount",
-  "onBeforeUpdate",
-  "onBeforeUnmount",
-], Vue);
+for (const k in Vue) {
+  window[k] = Vue[k];
+}
+const portal = proxyCall(
+  [
+    "onMounted",
+    "onUpdated",
+    "onUnmounted",
+    "onBeforeMount",
+    "onBeforeUpdate",
+    "onBeforeUnmount",
+  ],
+  Vue,
+);
 document.addEventListener("DOMContentLoaded", () => {
   parseTemplate();
   const scriptText = getSetupScriptText();
