d5b11b8d17275689ecc5d3c46ebcee4117f8c4a3
Tofu-Xx <1714843907@qq.com>
2024-10-07 04:38:14 +0800

    feat: proxyCall record ret

--- src/tools/proxyCall.ts
+++ src/tools/proxyCall.ts
@@ -1,19 +1,28 @@
 type FnName = string
+
+type Args = any[]
+type Ret = any
+interface Call {
+  args: Args
+  ret: Ret
+}
+
 export function proxyCall(fnNameList: FnName[], truthThis: object, proxyThis: object = window) {
-  const funPocket: { [key: string]: any[][] } = {}
+  const funPocket: Record<FnName, Call[]> = {}
   const _transport = (fnName: string) => {
     funPocket[fnName] = []
     proxyThis[fnName] = (...args) => {
-      funPocket[fnName].push(args)
+      funPocket[fnName].push({ args, ret: null })
     }
     return proxyThis[fnName]
   }
   fnNameList.forEach(k => proxyThis[k] = _transport(k))
+
   return {
     call() {
       Object.entries(funPocket).forEach(([fnName, calls]) => {
-        calls.forEach((args) => {
-          truthThis[fnName](...args)
+        calls.forEach((call) => {
+          call.ret = truthThis[fnName](...call.args)
         })
         proxyThis[fnName] = truthThis[fnName]
       })
