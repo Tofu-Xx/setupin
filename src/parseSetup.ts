6151c5e8723b591ecfa019167ddcbaa1efbf5f3e
Tofu-Xx <1714843907@qq.com>
2024-10-15 15:03:16 +0800

    feat: about import

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -2,10 +2,15 @@ export function parseSetup(setupScript: HTMLScriptElement) {
   const varRex = /(?:let|const|function|var)\s+\[?\{?\s*([a-zA-Z_$][\w$,\s]*)\b/g
   const localAreaRex = /\{[^{}]*\b(?:let|const|var|function)\b[^{}]*\}/g
   const commentsRex = /\/\*([^*]|\*[^/])*\*\/|\/\/.*/g
-  const setupText = setupScript.textContent!
+  const importsRex = /import[^\(].+/g
+  let setupText = setupScript.textContent!
   setupScript.remove()
-  const setupglobalText = setupText.replace(localAreaRex, '').replace(commentsRex, '')
-  const retNames = Array.from(setupglobalText.matchAll(varRex), ([_, v]) => v).join(',')
+  setupText = setupText.replace(localAreaRex, '').replace(commentsRex, '')
+  if(importsRex.test(setupText)){
+    setupText = setupText.replace(importsRex, '')
+    console.warn('Cannot use import statement outside a module.')
+  }
+  const retNames = Array.from(setupText.matchAll(varRex), ([_, v]) => v).join(',')
   return {
     setup: new Function(`for (const k in Vue) window[k] = Vue[k]; ${setupText} return { ${retNames} }`),
   }
