15ca74afa36bbfa8ab437732e8dd73b6ee088e3f
Tofu-Xx <1714843907@qq.com>
2024-10-19 23:18:06 +0800

    style: update

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -2,11 +2,11 @@ export function parseSetup(setupScript: HTMLScriptElement) {
   const varRex = /(?:let|const|function|var)\s+\[?\{?\s*([a-zA-Z_$][\w$,\s]*)\b/g
   const localAreaRex = /\{[^{}]*\b(?:let|const|var|function)\b[^{}]*\}/g
   const commentsRex = /\/\*([^*]|\*[^/])*\*\/|\/\/.*/g
-  const importsRex = /import[^\(].+/g
+  const importsRex = /import[^(].+/g
   let setupText = setupScript.textContent!
   setupScript.remove()
   setupText = setupText.replace(localAreaRex, '').replace(commentsRex, '')
-  if(importsRex.test(setupText)){
+  if (importsRex.test(setupText)) {
     setupText = setupText.replace(importsRex, '')
     console.warn('Cannot use import statement outside a module.')
   }
