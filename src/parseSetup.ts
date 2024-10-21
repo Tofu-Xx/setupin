243f04f6b03ce6538f24290cd5c4a20617f44b33
Tofu-Xx <1714843907@qq.com>
2024-10-22 02:39:47 +0800

    fix: nested

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -1,11 +1,34 @@
+function getNested(text: string): string[] {
+  let level = 0
+  const result: string[] = []
+  let current = ''
+
+  for (const char of text) {
+    if (char === '{' && level++)
+      current += level > 1 ? char : ''
+    else if (char === '}' && --level)
+      current += level ? char : ''
+    else if (level)
+      current += char
+
+    if (level === 0 && current) {
+      result.push(current)
+      current = ''
+    }
+  }
+
+  return result
+}
+
 export function parseSetup(setupScript: HTMLScriptElement) {
   const varRex = /(?:let|const|function|var)(\s+|\s+(\[|\{)\s*)([a-zA-Z_$][\w$,\s]*)\b/g
-  const localAreaRex = /\{[^{}]*\b(?:let|const|var|function)\b[^{}]*\}/g
   const commentsRex = /\/\*([^*]|\*[^/])*\*\/|\/\/.*/g
   const importsRex = /import[^(].+/g
-  const setupText = setupScript.textContent!
+  const setupText = setupScript.textContent!.replace(commentsRex, '')
   setupScript.remove()
-  let globalAreasText = setupText.replace(localAreaRex, '').replace(commentsRex, '')
+  let globalAreasText = getNested(setupText).filter(text => /let|const|var|function/.test(text)).reduce((prev, curr) => {
+    return prev.replace(curr, '')
+  }, setupText)
   if (importsRex.test(globalAreasText)) {
     globalAreasText = globalAreasText.replace(importsRex, '')
     console.warn('Cannot use import statement outside a module.')
