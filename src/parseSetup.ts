641ca5e88509e114735ff219f6f809781d8f2019
Tofu-Xx <1714843907@qq.com>
2024-10-28 18:59:14 +0800

    refactor(wip): when

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -23,36 +23,8 @@ function getGlobalVars(code: string): string[] {
 
   // 通用的模式处理函数
   function patterner(pattern: any, variables: string[]) {
-    // if (!pattern)
-    //   return
-
-    // switch (pattern.type) {
-    //   case 'ObjectPattern':
-    //     pattern.properties.forEach((prop: any) => {
-    //       patterner(prop.value, variables)
-    //     })
-    //     break
-
-    //   case 'ArrayPattern':
-    //     pattern.elements.forEach((element: any) => {
-    //       patterner(element, variables)
-    //     })
-    //     break
-
-    //   case 'Identifier':
-    //     variables.push(pattern.name)
-    //     break
-
-    //   case 'AssignmentPattern':
-    //     if (pattern.left.type === 'Identifier') {
-    //       variables.push(pattern.left.name)
-    //     }
-    //     break
-    // }
     when(pattern?.type)({
       ObjectPattern() {
-        //     pattern.properties.forEach((prop: any) => {
-        //     })
         for (const prop of pattern.properties)
           patterner(prop.value, variables)
       },
@@ -73,25 +45,31 @@ function getGlobalVars(code: string): string[] {
 
   return ast.program.body.reduce((variables: string[], node: any) => {
     // 处理变量声明
-    if (node.type === 'VariableDeclaration') {
-      for (const declaration of node.declarations) {
-        if (declaration.id.type === 'ObjectPattern') {
-          patterner(declaration.id, variables)
-        }
-        // 处理数组解构赋值
-        else if (declaration.id.type === 'ArrayPattern') {
-          patterner(declaration.id, variables)
-        }
-        // 处理普通变量声明
-        else if (declaration.id.type === 'Identifier') {
-          variables.push(declaration.id.name)
-        }
-      }
-    }
+    // if (node.type === 'VariableDeclaration') {
+
+    // }
     // 处理函数声明
-    else if (node.type === 'FunctionDeclaration' && node.id) {
-      variables.push(node.id.name)
-    }
+    // else if (node.type === 'FunctionDeclaration' && node.id) {
+    //   variables.push(node.id.name)
+    // }
+    when(node.type)({
+      FunctionDeclaration: () => variables.push(node.id.name),
+      VariableDeclaration: () => {
+        for (const declaration of node.declarations) {
+          if (declaration.id.type === 'ObjectPattern') {
+            patterner(declaration.id, variables)
+          }
+          // 处理数组解构赋值
+          else if (declaration.id.type === 'ArrayPattern') {
+            patterner(declaration.id, variables)
+          }
+          // 处理普通变量声明
+          else if (declaration.id.type === 'Identifier') {
+            variables.push(declaration.id.name)
+          }
+        }
+      },
+    })
     return variables
   }, [])
 }
