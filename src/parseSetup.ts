b813c2a0aac222cb20ebf00494a4193750c2d0f7
Tofu-Xx <1714843907@qq.com>
2024-10-28 16:52:25 +0800

    fix: arrayPatterner

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -21,11 +21,11 @@ function getGlobalVars(code: string): string[] {
   }
 
   // 辅助函数：处理对象解构模式
-  function processObjectPattern(pattern: any, variables: string[]) {
+  function objectPatterner(pattern: any, variables: string[]) {
     pattern.properties.forEach((prop: any) => {
       // 如果值是嵌套的对象解构
       if (prop.value.type === 'ObjectPattern') {
-        processObjectPattern(prop.value, variables)
+        objectPatterner(prop.value, variables)
       }
       // 如果是带默认值的解构
       else if (prop.value.type === 'AssignmentPattern') {
@@ -38,29 +38,36 @@ function getGlobalVars(code: string): string[] {
     })
   }
 
+  // 辅助函数：处理数组解构模式
+  function arrayPatterner(pattern: any, variables: string[]) {
+    pattern.elements.forEach((element: any) => {
+      if (!element)
+        return // 跳过空元素
+
+      if (element.type === 'Identifier') {
+        variables.push(element.name)
+      }
+      else if (element.type === 'ArrayPattern') {
+        arrayPatterner(element, variables)
+      }
+      // 可以在这里添加其他类型的处理，比如对象解构
+      else if (element.type === 'ObjectPattern') {
+        objectPatterner(element, variables)
+      }
+    })
+  }
+
   return ast.program.body.reduce((variables: string[], node: any) => {
     // 处理变量声明
     if (node.type === 'VariableDeclaration') {
       node.declarations.forEach((declaration: any) => {
         // 处理对象解构赋值
         if (declaration.id.type === 'ObjectPattern') {
-          processObjectPattern(declaration.id, variables)
+          objectPatterner(declaration.id, variables)
         }
         // 处理数组解构赋值
         else if (declaration.id.type === 'ArrayPattern') {
-          declaration.id.elements.forEach((element: any) => {
-            if (element.type === 'Identifier') {
-              variables.push(element.name)
-            }
-            // 处理嵌套数组解构
-            else if (element.type === 'ArrayPattern') {
-              element.elements.forEach((nestedElement: any) => {
-                if (nestedElement && nestedElement.type === 'Identifier') {
-                  variables.push(nestedElement.name)
-                }
-              })
-            }
-          })
+          arrayPatterner(declaration.id, variables)
         }
         // 处理普通变量声明
         else if (declaration.id.type === 'Identifier') {
