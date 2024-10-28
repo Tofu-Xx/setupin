1ecf2a47898bb7ae61b9b63abb07baf7ebbfd3e8
Tofu-Xx <1714843907@qq.com>
2024-10-28 11:21:51 +0800

    wip: ast

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -3,9 +3,13 @@ let hasSetup = false
 export function parseSetup(setupScript: HTMLScriptElement) {
   hasSetup = true
   const setupText = setupScript.textContent || ''
+  if (/import[^(].+/.test(setupText)) {
+    throw new Error('Cannot use import statement outside a module.')
+  }
   setupScript.remove()
+  const retNames = parseSetupText(setupText)
   return {
-    setup: new Function(`for (const k in Vue) window[k] = Vue[k]; ${setupText} return { ${paresSetupText(setupText)} }`),
+    setup: new Function(`for (const k in Vue) window[k] = Vue[k]; ${setupText} return { ${retNames} }`),
   }
 }
 
@@ -13,18 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
   hasSetup || console.warn('No setup found')
 })
 
-function paresSetupText(setupText: string) {
-  const commentsRex = /\/\*([^*]|\*[^/])*\*\/|\/\/.*/g
+function parseSetupText(setupText: string) {
   const stringRex = /(['"`])(?:(?!\1)[^\\]|\\.)*?\1/g
+  const commentsRex = /\/\*([^*]|\*[^/])*\*\/|\/\/.*/g
   const varRex = /(?:let|const|function|var)(\s+|\s+(\[|\{)\s*)([a-zA-Z_$][\w$,\s]*)\b/g
   /* 获取顶层域 */
-  let globalAreasText = getNested(setupText).filter(text => /let|const|var|function/.test(text)).reduce((prev, curr) => prev.replace(curr, ''), setupText)
-  if (/import[^(].+/.test(globalAreasText)) {
-    throw new Error('Cannot use import statement outside a module.')
-  }
+
+  const globalAreasText = getNested(setupText).filter(text => /let|const|var|function/.test(text)).reduce((prev, curr) => prev.replace(curr, ''), setupText)
+
   /* 排除注释和字符串 */
-  globalAreasText = globalAreasText.replace(commentsRex, '').replace(stringRex, '')
-  return Array.from(globalAreasText.matchAll(varRex), el => el[3]).join(',')
+  const neatText = globalAreasText.replace(stringRex, '\n').replace(commentsRex, '\n')
+  return Array.from(neatText.matchAll(varRex), el => el[3]).join(',')
 }
 
 function getNested(code: string): string[] {
