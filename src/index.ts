b69ae31a8ff9c3152e0fe27fba08fd471aec4a46
Tofu-Xx <1714843907@qq.com>
2024-10-31 03:29:14 +0800

    chore: vue asset

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
-// import * as esmVue from 'https://unpkg.com/vue/dist/vue.esm-browser.js'
-// import { ref } from 'vue'
-// import { Vue } from './assets/vue'
 import { observe } from './observe'
 import { parseSetup } from './parseSetup'
 import { parseTemplate } from './parseTemplate'
 import { loaded } from './tools'
-
-// console.log(ref)
-// console.log(Vue)
-// console.log(esmVue)
 
 const observed = observe({
   'script[setup]': parseSetup,
   'body': parseTemplate,
 })
 
-// console.log(esmVue, Vue, vue)
-
 loaded(async () => {
-  const [App, Template] = await Promise.all(Object.values(observed))
+  const [{ setupText, retNames }, Template] = await Promise.all(Object.values(observed))
   const Vue = await import('https://unpkg.com/vue/dist/vue.esm-browser.prod.js')
   ;(window as any).Vue = Vue
-  ;(Vue as any).createApp(App).mount(Template)
+  ;(Vue as any).createApp({
+    setup: new Function(`const { ${Object.keys(Vue)} } = Vue; ${setupText} return { ${retNames} }`),
+  }).mount(Template)
 })
--- src/parseSetup/index.ts
+++ src/parseSetup/index.ts
@@ -1,13 +1,13 @@
-import { Vue } from '../assets/vue'
 import { getASTBody, getGlobalVars, splitImport } from './parseAST'
 
-export function parseSetup(setupScript: HTMLScriptElement) {
+export async function parseSetup(setupScript: HTMLScriptElement) {
   const setupText = setupScript.textContent ?? ''
   const astBody = getASTBody(setupText)
   /* const rawImports = */ splitImport(setupText, astBody)
   // console.log(rawImports)
   setupScript.remove()
   return {
-    setup: new Function(`const { ${Object.keys(Vue)} } = Vue; ${setupText} return { ${getGlobalVars(astBody)} }`),
+    setupText,
+    retNames: getGlobalVars(astBody),
   }
 }
