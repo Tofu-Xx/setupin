cd1d279a8ab09e08d4dabd8cd2cc6e1196361273
Tofu-Xx <1714843907@qq.com>
2024-10-30 08:17:35 +0800

    refactor: when

--- src/observe.ts
+++ src/observe.ts
@@ -3,11 +3,11 @@ import { loaded, when } from './tools'
 export function observe(selector: string, callback: (target: any) => any): any
 export function observe(map: Record<string, (target: any) => any>): any
 export function observe(SorM: string | Record<string, (target: any) => any>, callback?: (target: any) => any) {
-  return when(typeof SorM)({
-    string: () => new Promise((resolve, reject) => {
+  return when(SorM, typeof SorM)({
+    string: selector => new Promise((resolve, reject) => {
       new MutationObserver((mutations, observer) => {
         for (const mutation of mutations) {
-          const target = (mutation.target as HTMLElement)?.querySelector(SorM as string)
+          const target = (mutation.target as HTMLElement)?.querySelector(selector)
           if (target && callback) {
             observer.disconnect()
             const result = callback(target)
@@ -19,16 +19,16 @@ export function observe(SorM: string | Record<string, (target: any) => any>, cal
         }
         loaded(() => {
           observer.disconnect()
-          reject(new Error(`No ${SorM} found`))
+          reject(new Error(`No ${selector} found`))
         })
       }).observe(document, {
         childList: true,
         subtree: true,
       })
     }),
-    object: () => Object.keys(SorM).reduce((prev, key) => ({
+    object: map => Object.keys(map).reduce((prev, key) => ({
       ...prev,
-      [key]: observe(key, (SorM as Record<string, (target: any) => any>)[key]),
+      [key]: observe(key, map[key]),
     }), Object.create(null)),
   })
 }
--- src/tools.ts
+++ src/tools.ts
@@ -1,7 +1,30 @@
-export function when(unknow: any) {
-  return (types: Record<string, any>) => types[unknow in types ? unknow : 'default']?.()
+type Handler<T> = (value: T) => any
+type Classify<K extends string | number | symbol> = {
+  [P in K]?: Handler<any>
+} & {
+  [key: symbol]: Handler<any>
+}
+export function when<V extends string | number | symbol>(gist: any, verifyer: V = gist) {
+  return function (classify: Classify<V>) {
+    const another = Object.getOwnPropertySymbols(classify).find(sym => sym.description === 'default') ?? Symbol('default')
+    const handler = classify[verifyer] ?? classify[another]
+    return handler?.(gist)
+  }
 }
 
+// // /*  */
+// const v: string | any[] = 'ye'
+// when(v)({
+//   yes() {
+//     console.log('yes')
+//   },
+//   no() {
+//     console.log('no')
+//   },
+//   [Symbol('default')]: () => {
+//     console.log('default')
+//   },
+// })
 export function loaded(callback: () => void) {
   document.addEventListener('DOMContentLoaded', callback)
 }
