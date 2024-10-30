cc8b6fd6d1aa5520e8b3d5247e268e1f1ca9e369
Tofu-Xx <1714843907@qq.com>
2024-10-30 14:09:48 +0800

    refactor: getGlobalVars give up reduce

--- src/parseSetup.ts
+++ src/parseSetup.ts
@@ -14,42 +14,28 @@ export function parseSetup(setupScript: HTMLScriptElement) {
 function getGlobalVars(code: string): string[] {
   const ast = parse(code, { sourceType: 'script' })
 
-  return ast.program.body.reduce((prev: string[], node) => {
-    when(node, node.type)({
-      ImportDeclaration() { throw new Error('Cannot use import statement outside a module.') },
-      FunctionDeclaration({ id }: FunctionDeclaration) {
-        prev.push(id?.name ?? '')
-      },
-      VariableDeclaration(n: VariableDeclaration) {
-        for (const { id } of n.declarations)
-          prev.push(...patterner(id))
-      },
-    })
-    return prev
-  }, [])
+  return ast.program.body.flatMap(node => when(node, node.type)({
+    ImportDeclaration() { throw new Error('Cannot use import statement outside a module.') },
+    FunctionDeclaration: ({ id }: FunctionDeclaration) => [id?.name ?? ''],
+    VariableDeclaration: (n: VariableDeclaration) => n.declarations.map(({ id }) => patterner(id)),
+  }))
 
   function patterner(pattern: LVal): string[] {
-    const rest = (vars: string[]) => ({ RestElement: (p: RestElement) => vars.concat(patterner(p.argument)) })
+    const RestElement = (p: RestElement) => patterner(p.argument)
     return when(pattern, pattern?.type)({
       Identifier: (p: Identifier) => [p.name],
       AssignmentPattern: (p: AssignmentPattern) => patterner(p.left),
-      ObjectPattern(p: ObjectPattern) {
-        return p.properties.reduce((prev: string[], prop) => {
-          return when(prop, prop.type)({
-            ObjectProperty: (p: ObjectProperty) => prev.concat(patterner(p.value as LVal)),
-            ...rest(prev),
-          })
-        }, [])
-      },
-      ArrayPattern(p: ArrayPattern) {
-        return p.elements.reduce((prev: string[], element) => {
-          return when(element, element?.type ?? 0)({
-            0: () => prev,
-            ...rest(prev),
-            [Symbol('default')]: (el: Pattern) => prev.concat(patterner(el)),
-          })
-        }, [])
-      },
+      ObjectPattern: (p: ObjectPattern) =>
+        p.properties.flatMap(prop => when(prop, prop.type)({
+          ObjectProperty: (p: ObjectProperty) => patterner(p.value as LVal),
+          RestElement,
+        }), []),
+      ArrayPattern: (p: ArrayPattern) =>
+        p.elements.flatMap(element => when(element, element?.type ?? 0)({
+          0: () => [],
+          RestElement,
+          [Symbol('default')]: (el: Pattern) => patterner(el),
+        })),
     })
   }
 }
