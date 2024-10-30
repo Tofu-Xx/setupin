37fe460ee5a7612a7706785a5113b2f94ef7da4d
Tofu-Xx <1714843907@qq.com>
2024-10-30 09:00:18 +0800

    refactor!: when type

--- src/tools.ts
+++ src/tools.ts
@@ -1,9 +1,12 @@
 type Fn<T> = (arg: T) => any
 type Classify<K extends string | number | symbol> = Partial<Record<K, Fn<any>> & Record<symbol, Fn<any>>>
-export function when<V extends string | number | symbol>(gist: any, verifyer: V = gist) {
+export function when<V extends string | number | symbol>(gist: V): Fn<Classify<V>>
+export function when<V extends string | number | symbol>(gist: any, verifyer: V): Fn<Classify<V>>
+export function when<V extends string | number | symbol>(gist: any, verifyer?: V) {
+  const fnname: V = verifyer ?? gist
   return function (classify: Classify<V>) {
     const another = Object.getOwnPropertySymbols(classify).find(sym => sym.description === 'default') ?? Symbol('default')
-    const handler = classify[verifyer] ?? classify[another]
+    const handler = classify[fnname] ?? classify[another]
     return handler?.(gist)
   }
 }
