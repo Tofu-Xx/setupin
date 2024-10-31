171612bbeb2092ed242193be2f75d04f62d06ed4
Tofu-Xx <1714843907@qq.com>
2024-10-31 20:56:47 +0800

    feat!: async comp

--- src/generateCode/index.ts
+++ src/generateCode/index.ts
@@ -7,17 +7,24 @@ export function generateCode(
     isAsync,
   }: Context,
 ) {
-  return `
-  ${importCode}
-  const { ${Object.keys(window.Vue)} } = Vue
-  createApp({
+  const suspense = `{
+    components: { AsyncComp },
+    template: \`<Suspense><AsyncComp /></Suspense>\`,
+  }`
+  const app = `{
     template: \`${template}\`,
-    ${isAsync ? 'async' : ''} setup(){
+    ${isAsync ? 'async' : ''} setup() {
       ${setupCode}
       return {
         ${retNames}
       }
-    }
-  }).mount(document.body)
+    },
+  }`
+  const statementAsync = isAsync ? `const AsyncComp = ${app}` : ''
+  return `
+  ${importCode}
+  const { ${Object.keys(window.Vue)} } = Vue;
+  ${statementAsync}
+  createApp(${isAsync ? suspense : app}).mount(document.body);
   `
 }
