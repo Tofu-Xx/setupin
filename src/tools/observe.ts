f148d915ac946a5ed13b824db3c7f5c1d8a8f6a7
Tofu-Xx <1714843907@qq.com>
2024-10-31 23:08:38 +0800

    fix: Multiple trigger DOMContentLoaded

--- src/tools/observe.ts
+++ src/tools/observe.ts
@@ -2,7 +2,7 @@ type Ret<R extends Fn> = Promise<Exclude<ReturnType<R>, Error>>
 export function observe<R extends Fn>(selector: string, callback: R): Ret<R> {
   return new Promise((resolve, reject) => {
     let isFound = false
-    new MutationObserver((mutations, observer) => {
+    const observer = new MutationObserver((mutations, observer) => {
       for (const mutation of mutations) {
         const target = (mutation.target as HTMLElement)?.querySelector(selector)
         if (target) {
@@ -12,22 +12,23 @@ export function observe<R extends Fn>(selector: string, callback: R): Ret<R> {
           break
         }
       }
-      document.addEventListener('DOMContentLoaded', () => {
-        if (isFound)
-          return
-        observer.disconnect()
-        const result = callback?.(void 0)
-        if (result instanceof Error) {
-          reject(result)
-        }
-        else {
-          resolve(result)
-          console.warn(`No ${selector} found`)
-        }
-      })
-    }).observe(document, {
+    })
+    observer.observe(document, {
       childList: true,
       subtree: true,
+    })
+    document.addEventListener('DOMContentLoaded', () => {
+      if (isFound)
+        return
+      observer.disconnect()
+      const result = callback?.(void 0)
+      if (result instanceof Error) {
+        reject(result)
+      }
+      else {
+        resolve(result)
+        console.warn(`No ${selector} found`)
+      }
     })
   })
 }
