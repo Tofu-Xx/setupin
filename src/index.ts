6baa781db4d2aff6f5e8b59bc5b00c817077abad
Tofu-Xx <1714843907@qq.com>
2024-10-31 22:14:38 +0800

    chore: use vue asset

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
+import { Vue } from './assets/vue'
 import { generateCode } from './generateCode'
 import { parseSetup } from './parseSetup'
 import { parseTemplate } from './parseTemplate'
 import { observe } from './tools'
 
+window.Vue = Vue as Vue
 const oTemplate = observe('head>template', parseTemplate)
 const oSetup = observe('script[setup]', parseSetup)
 
 ;(async () => {
-  const template = await oTemplate
-  const { setupEl, context } = await oSetup
-  window.Vue = await import('https://unpkg.com/vue/dist/vue.esm-browser.prod.js')
+  const [{ setupEl, context }, template] = await Promise.all([oSetup, oTemplate])
   setupEl.textContent = generateCode(template, context)
 })()
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,9 @@
 declare module 'https://unpkg.com/vue/dist/vue.esm-browser.prod.js' {
   export * from 'vue'
 }
+declare type Vue = typeof import('https://unpkg.com/vue/dist/vue.esm-browser.prod.js')
 declare interface Window {
-  Vue: typeof import('https://unpkg.com/vue/dist/vue.esm-browser.prod.js')
+  Vue: Vue
 }
 /* tool types */
 declare type Fn<T extends any[] = [any], R = any> = (...args: T) => R
