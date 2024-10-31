9b808ce717b5d2c51ee59a424ee20fd7ebfb13f4
Tofu-Xx <1714843907@qq.com>
2024-10-31 11:52:11 +0800

    chore: ok

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,26 @@ import { observe } from './observe'
 import { parseSetup } from './parseSetup'
 import { parseTemplate } from './parseTemplate'
 
-const oSetup = observe('script[setup]', parseSetup)
 const oTemplate = observe('head>template', parseTemplate)
+const oSetup = observe('script[setup]', parseSetup)
 
-// ;(async () => {
- 
-// })()
-const template = await oTemplate
-const { setupEl, importText, setupText, retNames } = await oSetup
-setupEl.textContent = `
-import * as Vue from 'https://unpkg.com/vue/dist/vue.esm-browser.prod.js'
-${importText}
-for (const k in Vue) {
-  window[k] = Vue[k]
-}
-Vue.createApp({
-  template: \`${template}\`,
-  setup(){
-    ${setupText}
-    return {
-      ${retNames}
-    }
+;(async () => {
+  const template = await oTemplate
+  const { setupEl, importText, setupText, retNames } = await oSetup
+  setupEl.textContent = `
+  import * as Vue from 'https://unpkg.com/vue/dist/vue.esm-browser.prod.js'
+  ${importText}
+  for (const k in Vue) {
+    window[k] = Vue[k]
   }
-}).mount(document.body)
-`
+  Vue.createApp({
+    template: \`${template}\`,
+    setup(){
+      ${setupText}
+      return {
+        ${retNames}
+      }
+    }
+  }).mount(document.body)
+  `
+})()
