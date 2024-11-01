050e161bdd4e7c63ecff8b4d32032886b1d0e813
Tofu-Xx <1714843907@qq.com>
2024-11-02 01:26:55 +0800

    wip: mount body

--- src/monito/parseTemplate.ts
+++ src/monito/parseTemplate.ts
@@ -1,5 +1,8 @@
 export function parseTemplate(templateEl: Tag['template'] | undefined) {
   const templateContent = templateEl?.innerHTML ?? '{{"not found <template>"}}'
   templateEl?.remove()
+  // const body = document.body ?? document.createElement('body')
+  // body.innerHTML = templateContent
+  // document.head.parentNode?.insertBefore(body, document.head.nextSibling)
   return templateContent?.replace(/[`"'$\\]/g, s => `\\${s}`)
 }
