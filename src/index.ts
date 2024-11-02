c7c786860cf81a3a7d103c0aa7c2e9f95bb8a052
Tofu-Xx <1714843907@qq.com>
2024-11-02 14:29:22 +0800

    refactor: monito

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,49 @@
+import type { Data } from './monito'
+import type { Result } from './result'
 import { generate } from './generate'
 import { monito } from './monito'
+import { parseScript } from './monito/parseScript'
+import { parseTemplate } from './monito/parseTemplate'
+import { Err, Ok } from './result'
+import { when } from './tools'
 
-const monitored = monito()
+const finder = (() => {
+  const data: Data = [null, null] as unknown as Data
+  return function (fn: Fn<any, Result>, target: Element | string, resolve: Fn) {
+    const [i, type] = fn === parseScript ? [0, 'warn'] : [1, 'error']
+    const arg = typeof target === 'string' ? null : target
+    const { ok, isErr } = fn(arg)
+    if (isErr)
+      return
+    data[i] = ok
+    if (!arg) {
+      ;(console as any)[type](`not found ${target} in root node`)
+    }
+    if (data.every(e => e !== null))
+      resolve(data)
+  }
+})()
+function isSite(node: Node, tagName: string, attrName?: string) {
+  if (!(node instanceof Element))
+    return new Err(`${node} is not element`)
+  const isTagName = tagName === node.localName
+  const isAttrName = attrName ? node.getAttributeNames().includes(attrName) : true
+  if (isTagName && isAttrName)
+    return new Ok(node.parentElement === document.head)
+  return new Err(`${tagName} ${attrName ? `with ${attrName}` : ''} not found`)
+}
+
+const monitored = monito(({ node, resolve }) => {
+  isSite(node, 'template').ok && finder(parseTemplate, node as Element, resolve)
+  when(node, isSite(node, 'script', 'setup').ok)({
+    true: (n: Element) => finder(parseScript, n, resolve),
+    false: (n: Element) => (n).textContent = '/* Resolved to the wrong location */',
+  })
+}, ({ observer, resolve }) => {
+  finder(parseScript, '<script setup>', resolve)
+  finder(parseTemplate, '<template>', resolve)
+  observer.disconnect()
+})
 
 ;(async () => {
   const [{ scriptEl, context }, templateContent] = await monitored
--- src/monito/index.ts
+++ src/monito/index.ts
@@ -1,36 +1,19 @@
 import type { ParsedScript } from './parseScript'
 import type { ParsedTemplate } from './parseTemplate'
-import { Err, Ok, type Result } from '../result'
 
-import { when } from '../tools'
-import { parseScript } from './parseScript'
-import { parseTemplate } from './parseTemplate'
-
-type Data = [ParsedScript, ParsedTemplate]
-export function monito(): Promise<Data> {
-  const data: Data = [null, null] as unknown as Data
-  return new Promise((resolve) => {
-    function _finder(fn: Fn<any, Result>, target: Element | string) {
-      const [i, type] = fn === parseScript ? [0, 'warn'] : [1, 'error']
-      const arg = typeof target === 'string' ? null : target
-      const { ok, isErr } = fn(arg)
-      if (isErr)
-        return
-      data[i] = ok
-      if (!arg) {
-        ;(console as any)[type](`not found ${target} in root node`)
-      }
-      if (data.every(e => e !== null))
-        resolve(data)
-    }
-    const observer = new MutationObserver((mutations) => {
+interface Supply {
+  node: Node
+  observer: MutationObserver
+  resolve: (value: Data | PromiseLike<Data>) => void
+  reject: (reason?: any) => void
+}
+export type Data = [ParsedScript, ParsedTemplate]
+export function monito(monitor: Fn<[Supply]>, loaded?: Fn<[Omit<Supply, 'node'>]>): Promise<Data> {
+  return new Promise((resolve, reject) => {
+    const observer = new MutationObserver((mutations, observer) => {
       for (const mutation of mutations) {
         mutation.addedNodes.forEach((node) => {
-          isElSite(node, 'template').ok && _finder(parseTemplate, node as Element)
-          when(node, isElSite(node, 'script', 'setup').ok)({
-            true: (n: Element) => _finder(parseScript, n),
-            false: (n: Element) => (n).textContent = '/* Resolved to the wrong location */',
-          })
+          monitor({ node, observer, resolve, reject })
         })
       }
     })
@@ -39,26 +22,7 @@ export function monito(): Promise<Data> {
       subtree: true,
     })
     document.addEventListener('DOMContentLoaded', () => {
-      _finder(parseScript, '<script setup>')
-      _finder(parseTemplate, '<template>')
-      observer.disconnect()
+      loaded?.({ observer, resolve, reject })
     })
   })
-}
-// interface NodeInfo {
-//   tagName: string
-//   attrNames: string[]
-//   parentTagName: string
-// }
-// function getNodeInfo(node: Node): Option<NodeInfo> {
-//   }
-
-function isElSite(node: Node, tagName: string, attrName?: string) {
-  if (!(node instanceof Element))
-    return new Err(`${node} is not element`)
-  const isTagName = tagName === node.localName
-  const isAttrName = attrName ? node.getAttributeNames().includes(attrName) : true
-  if (isTagName && isAttrName)
-    return new Ok(node.parentElement === document.head)
-  return new Err(`${tagName} ${attrName ? `with ${attrName}` : ''} not found`)
 }
