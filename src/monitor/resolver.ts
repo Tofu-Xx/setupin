3b7f4974f402e7620e00ea503dda09a58fa1adf7
Tofu-Xx <1714843907@qq.com>
2024-11-03 04:34:28 +0800

    style: iife?

--- src/monitor/resolver.ts
+++ src/monitor/resolver.ts
@@ -1,13 +1,14 @@
 import { bus, byBus, stops } from '@/doms/bus'
 import { root } from '@/doms/root'
 
-export const resolver = (() => {
-  return function (tag: ROOT_TAG, resolve: Fn, target?: Element) {
-    const { doBy, lose } = root[tag]
-    if (byBus(tag, doBy(target as any))) {
-      target || lose()
-      if (stops() === 2)
-        resolve(bus)
-    }
+// export const resolverFactory = (() => {
+
+// })()
+export function resolver(tag: ROOT_TAG, resolve: Fn, target?: Element) {
+  const { doBy, lose } = root[tag]
+  if (byBus(tag, doBy(target as any))) {
+    target || lose()
+    if (stops() === 2)
+      resolve(bus)
   }
-})()
+}
