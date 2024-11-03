8236b5752d59df55c64c2c5a1ead9bc09f2235a5
Tofu-Xx <1714843907@qq.com>
2024-11-03 12:49:11 +0800

    feat: On-demand import

--- src/funcs/ast/extractImport.ts
+++ src/funcs/ast/extractImport.ts
@@ -3,7 +3,7 @@ import type { Statement } from '@babel/types'
 export function extractImport(astBody: Statement[], code: string) {
   const imports = astBody.filter(({ type }) => type === 'ImportDeclaration').map(node => code.slice(node.start!, node.end!))
   return {
-    importsCode: imports.map(i => `${i}\n`).join(''),
+    importsCode: `${imports.join('\n')}\n`,
     setupCode: `${code.replace(new RegExp(imports.join('|'), 'g'), '').trim()}\n`,
   }
 }
--- src/funcs/generate.ts
+++ src/funcs/generate.ts
@@ -1,14 +1,13 @@
 import * as Vue from 'vendor:vue'
 
+const demand = new RegExp(`\\b${Object.keys(window.Vue = Vue).join('\\b|\\b')}\\b`, 'g')
 export function generate(context: Context) {
   const { importsCode, setupCode, retNames, isAsync } = context
-  const appComp = `{
-    // template: \`\${template}\`,
-    ${isAsync ? 'async' : ''} setup() {${setupCode}return {${retNames}}},
-  }`
+  const async = isAsync ? 'async' : ''
+  const appComp = `{${async} setup() {${setupCode}return {${retNames}}}}`
   const suspenseComp = `{components:{c:${appComp}},template:'<Suspense><c/></Suspense>'}`
   const createApp = `createApp(${isAsync ? suspenseComp : appComp}).mount(document.body);`
-  const autoImport = `const { ${Object.keys(window.Vue = Vue)} } = Vue;`
+  const autoImport = `const { createApp,${setupCode.match(demand) ?? ''} } = Vue;`
   return importsCode
     + autoImport
     + createApp
