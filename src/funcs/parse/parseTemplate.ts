91137af11ab2bdc1142fcdc6bb2b0fbefa38929b
Tofu-Xx <1714843907@qq.com>
2024-11-03 11:37:11 +0800

    fix: &lt &gt

--- src/funcs/parse/parseTemplate.ts
+++ src/funcs/parse/parseTemplate.ts
@@ -1,6 +1,6 @@
 export type ParsedTemplate = ReturnType<typeof parseTemplate>
 export function parseTemplate(templateEl?: Maybe<Tag['template']>) {
-  const templateContent = templateEl?.innerHTML ?? '{{"not found <template>"}}'
+  const templateContent = templateEl?.innerHTML ?? 'not found &lt;template&gt;'
   templateEl?.remove()
   return templateContent
 }
