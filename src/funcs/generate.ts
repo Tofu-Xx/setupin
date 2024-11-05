0257402e8c93d32675d1fa1914ad78d0ebff7f7f
Tofu-Xx <1714843907@qq.com>
2024-11-05 12:22:57 +0800

    feat: ascii-logo

--- src/funcs/generate.ts
+++ src/funcs/generate.ts
@@ -1,8 +1,10 @@
 import type { ParsedScript, ParsedTemplate } from './parse'
+import asciiLogoRaw from '@/asset/ascii-logo.txt?raw'
 import * as Vue from 'vendor:vue'
 
 export function generate(templateCode: ParsedTemplate, context: ParsedScript) {
   document.body.innerHTML = templateCode
+  const asciiLogo = import.meta.env.MODE === 'development' ? asciiLogoRaw : ''
   const demandRex = new RegExp(`\\b${Object.keys(window.Vue = Vue).join('\\b|\\b')}\\b`, 'g')
   const { scriptEl, importsCode, setupCode, retNames, isAsync } = context
   const async = isAsync ? 'async' : ''
@@ -10,5 +12,5 @@ export function generate(templateCode: ParsedTemplate, context: ParsedScript) {
   const suspenseComp = `{components:{c:${appComp}},template:'<Suspense><c/></Suspense>'}`
   const createApp = `createApp(${isAsync ? suspenseComp : appComp}).mount(document.body);`
   const autoImport = `const {createApp,${[...new Set(setupCode.match(demandRex))]}}=Vue;`
-  scriptEl!.innerHTML = importsCode + autoImport + createApp
+  scriptEl!.innerHTML = asciiLogo + importsCode + autoImport + createApp
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import type { carrier } from '@/store/carrier'
 import { tagScript, tagTemplate } from '@/data'
 import { onAfter, onPrior } from '@/funcs/discoverer'
-import { generate } from '@/funcs/generate'
 import { discover } from '@/util'
+import { generate } from './funcs/generate'
 
 (async () => {
   const discovery = await discover<typeof carrier>(onPrior, onAfter)
