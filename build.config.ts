4340514fa693aab3caf34e400c46eeb0345c25a0
Tofu-Xx <1714843907@qq.com>
2024-11-07 06:19:06 +0800

    chore: definePreset

--- build.config.ts
+++ build.config.ts
@@ -1,16 +1,11 @@
 import { resolve } from 'node:path'
-import { defineBuildConfig } from 'unbuild'
+import { defineBuildConfig, definePreset } from 'unbuild'
 
 // process.env.NODE_ENV = 'production'
 // const prod = process.env.NODE_ENV === 'production'
-
-// console.log(prod)
-export default defineBuildConfig([
-  {
+function base() {
+  return definePreset({
     failOnWarn: false,
-    replace: {
-      __IS_PROD__: String(false),
-    },
     // clean: true,
     // declaration: true,
     entries: [{
@@ -22,54 +17,39 @@ export default defineBuildConfig([
       '@vendor': resolve(__dirname, 'vendor'),
     },
     rollup: {
-      // emitCJS: false, // 是否输出 CommonJS 格式
+    // emitCJS: false, // 是否输出 CommonJS 格式
       output: {
         format: 'iife', // 指定输出格式为 iife
         name: 'Vue', // 全局变量名
-        entryFileNames: '[name].js', // 输出文件名
-        // globals: {
-        //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
-        // },
+      // entryFileNames: '[name].prod.js', // 输出文件名
+      // globals: {
+      //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
+      // },
       },
       esbuild: {
         minify: true, // 是否压缩代码
-        // target: 'es2015', // 指定 esbuild 编译目标
-        // sourcemap: true, // 是否生成 sourcemap
+      // target: 'es2015', // 指定 esbuild 编译目标
+      // sourcemap: true, // 是否生成 sourcemap
       },
     },
 
-  },
-  {
-    failOnWarn: false,
-    replace: {
-      __IS_PROD__: String(true),
-    },
-    // clean: true,
-    // declaration: true,
-    entries: [{
-      input: 'src/main.ts',
-      name: 'main',
-    }],
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@vendor': resolve(__dirname, 'vendor'),
-    },
-    rollup: {
-      // emitCJS: false, // 是否输出 CommonJS 格式
-      output: {
-        format: 'iife', // 指定输出格式为 iife
-        name: 'Vue', // 全局变量名
-        entryFileNames: '[name].prod.js', // 输出文件名
-        // globals: {
-        //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
-        // },
-      },
-      esbuild: {
-        minify: true, // 是否压缩代码
-        // target: 'es2015', // 指定 esbuild 编译目标
-        // sourcemap: true, // 是否生成 sourcemap
-      },
-    },
+  })
+}
 
+export default defineBuildConfig([{
+  preset: base(),
+  replace: { __IS_PROD__: String(false) },
+  rollup: {
+    output: {
+      entryFileNames: '[name].js',
+    },
   },
-])
+}, {
+  preset: base(),
+  replace: { __IS_PROD__: String(true) },
+  rollup: {
+    output: {
+      entryFileNames: '[name].prod.js',
+    },
+  },
+}])
