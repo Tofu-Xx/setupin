2efc4616374e1ff821eee4cf6a24e8541cd577cd
Tofu-Xx <1714843907@qq.com>
2024-11-07 06:04:04 +0800

    chore: prod ok

--- build.config.ts
+++ build.config.ts
@@ -1,33 +1,75 @@
 import { resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
 
-export default defineBuildConfig({
-  failOnWarn: false,
-  // clean: true,
-  // declaration: true,
-  entries: [{
-    input: 'src/main.ts',
-    name: 'main',
-  }],
-  alias: {
-    '@': resolve(__dirname, 'src'),
-    '@vendor': resolve(__dirname, 'vendor'),
-  },
-  rollup: {
-    // emitCJS: false, // 是否输出 CommonJS 格式
-    output: {
-      format: 'iife', // 指定输出格式为 iife
-      name: 'Vue', // 全局变量名
-      entryFileNames: '[name].js', // 输出文件名
-      // globals: {
-      //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
-      // },
-    },
-    esbuild: {
-      minify: true, // 是否压缩代码
-      // target: 'es2015', // 指定 esbuild 编译目标
-      // sourcemap: true, // 是否生成 sourcemap
-    },
-  },
+// process.env.NODE_ENV = 'production'
+// const prod = process.env.NODE_ENV === 'production'
 
-})
+// console.log(prod)
+export default defineBuildConfig([
+  {
+    failOnWarn: false,
+    replace: {
+      __IS_PROD__: String(false),
+    },
+    // clean: true,
+    // declaration: true,
+    entries: [{
+      input: 'src/main.ts',
+      name: 'main',
+    }],
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@vendor': resolve(__dirname, 'vendor'),
+    },
+    rollup: {
+      // emitCJS: false, // 是否输出 CommonJS 格式
+      output: {
+        format: 'iife', // 指定输出格式为 iife
+        name: 'Vue', // 全局变量名
+        entryFileNames: '[name].js', // 输出文件名
+        // globals: {
+        //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
+        // },
+      },
+      esbuild: {
+        minify: true, // 是否压缩代码
+        // target: 'es2015', // 指定 esbuild 编译目标
+        // sourcemap: true, // 是否生成 sourcemap
+      },
+    },
+
+  },
+  {
+    failOnWarn: false,
+    replace: {
+      __IS_PROD__: String(true),
+    },
+    // clean: true,
+    // declaration: true,
+    entries: [{
+      input: 'src/main.ts',
+      name: 'main',
+    }],
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@vendor': resolve(__dirname, 'vendor'),
+    },
+    rollup: {
+      // emitCJS: false, // 是否输出 CommonJS 格式
+      output: {
+        format: 'iife', // 指定输出格式为 iife
+        name: 'Vue', // 全局变量名
+        entryFileNames: '[name].prod.js', // 输出文件名
+        // globals: {
+        //   vue: 'Vue', // 例如，如果依赖 vue，可以将其映射为 Vue 的全局变量
+        // },
+      },
+      esbuild: {
+        minify: true, // 是否压缩代码
+        // target: 'es2015', // 指定 esbuild 编译目标
+        // sourcemap: true, // 是否生成 sourcemap
+      },
+    },
+
+  },
+])
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { onAfter, onPrior } from '@/funcs/discoverer'
 import { generate } from '@/funcs/generate'
 import { discover } from '@/util'
 
+console.log(__IS_PROD__)
 export * from '@vendor/vue.esm'
 (async () => {
   const discovery = await discover<typeof carrier>(onPrior, onAfter)
