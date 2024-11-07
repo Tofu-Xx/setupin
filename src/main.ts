cb42518449745fc49ab543c0fffcfff043a1743b
Tofu-Xx <1714843907@qq.com>
2024-11-08 01:22:08 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { compileScript, compileStyle, compileTemplate } from 'vue/compiler-sfc'
-// import demoScript from './demo.vue?type=script'
+import { compileScript, compileStyle, compileTemplate, parse } from 'vue/compiler-sfc'
+import script from './demo.vue?vue&type=script'
+import { render } from './demo.vue?vue&type=template&id=xxxxxx'
+import './demo.vue?vue&type=style&index=0&id=xxxxxx'
+
+script.render = render
+script.__file = 'demo.vue'
+export default script
 // compileScript()
 // compileStyle()
 // compileTemplate()
