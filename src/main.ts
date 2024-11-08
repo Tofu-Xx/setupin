b834712be1f800622ca892867cf91c6a0b9f30a5
Tofu-Xx <1714843907@qq.com>
2024-11-08 18:12:25 +0800

    chore: look code

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,23 @@
 import { compileScript, compileStyle, compileTemplate, parse } from 'vue/compiler-sfc'
-const source = `
-<script setup lang="ts">
-const count = ref(0)
-</script>
+import { source } from './source'
 
-<template>
-  <button @click="count++">
-    {{ count }}
-  </button>
-</template>
-
-<style scoped>
-  button {
-    background-color: #6af;
-    color: #fff;
-    font-weight: bolder;
-  }
-</style>
-`
-/*  */
-const parsed = parse(source, {
-  filename: 'App.vue',
-})
-console.log('parsed', parsed)
-/* template */
-const compileredTemplate = compileTemplate({
-  id: 'setupin',
-  filename: 'App.vue',
+const id = 'setupin'
+const filename = 'App.vue'
+const parsed = parse(source)
+const SFCScriptBlock = compileScript(parsed.descriptor, { id })
+const SFCTemplateCompileResults = compileTemplate({
+  id,
+  filename,
   source: parsed.descriptor.template?.content ?? '',
 })
-console.log('template', compileredTemplate)
-/* script */
-const compileredScript = compileScript(parsed.descriptor, {
-  id: 'setupin',
-})
-console.log('script', compileredScript)
-/* style */
-const compileredStyles = parsed.descriptor.styles.map((style, i) => {
+const SFCStyleCompileResultsList = parsed.descriptor.styles.map((style) => {
   return compileStyle({
-    id: `setupin-style-${i + 1}`,
-    filename: 'App.vue',
+    id,
+    filename,
     source: style.content,
-    scoped: style.scoped,
   })
 })
-console.log('styles', compileredStyles)
+console.log(SFCScriptBlock.content)
+console.log(SFCTemplateCompileResults.code)
+console.log(SFCStyleCompileResultsList.map(style => style.code).join('\n'))
+// console.log('styles', compileredStyles)
