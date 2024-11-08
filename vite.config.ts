57834c535115d34e13627b867013a47fcb09e73e
Tofu-Xx <1714843907@qq.com>
2024-11-09 00:46:11 +0800

    chore: devDependencies vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from 'node:path'
-import vue from '@vitejs/plugin-vue'
+// import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  // plugins: [vue()],
   publicDir: false,
   resolve: {
     alias: [
