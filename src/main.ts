cfd082baaedb9e27387434fc0dc59c12fa86f859
Tofu-Xx <1714843907@qq.com>
2024-11-10 14:34:42 +0800

    chore: MagicString

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// import * as compilerSfc from 'vue/compiler-sfc'
+import { MagicString } from 'vue/compiler-sfc'
 import { compiler } from './compiler'
 import { getSource } from './getSource'
 import { watchRoot } from './utils';
@@ -6,14 +6,18 @@ import { watchRoot } from './utils';
 (async () => {
   const sources = await watchRoot(getSource)
   const { sfcScriptBlock, sfcStyleCompileResultsList, sfcTemplateCompileResults } = compiler(sources.join('\n'))
-  console.log(sfcScriptBlock)
-  console.log(sfcStyleCompileResultsList)
-  console.log(sfcTemplateCompileResults)
+  // console.log(sfcScriptBlock)
+  // console.log(sfcStyleCompileResultsList)
+  // console.log(sfcTemplateCompileResults)
+  /* template */
+  document.body.innerHTML = sfcTemplateCompileResults.source
+  /* style */
   const styleCode = sfcStyleCompileResultsList.map(style => style.code).join('\n')
   const styleEl = document.createElement('style')
-  styleEl.dataset.setupin = ''
   styleEl.innerHTML = styleCode
   document.head.appendChild(styleEl)
-  document.body.innerHTML = sfcTemplateCompileResults.source
-  // console.log(compiled)
+  /* script */
+  const s = new MagicString(sfcScriptBlock.content)
+  s.replace('export default', 'const App =')
+  console.log(s.toString())
 })()
