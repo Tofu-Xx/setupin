48d9d3b85ec3516557585906281f1c0467347f4f
Tofu-Xx <1714843907@qq.com>
2024-11-13 11:20:14 +0800

    fix: script src

--- src/funcs/awaitCompileSfc.ts
+++ src/funcs/awaitCompileSfc.ts
@@ -1,17 +1,18 @@
 import type { SFCScriptBlock, SFCStyleCompileResults, SFCTemplateCompileResults } from '@vue/compiler-sfc'
 import { compilerSfc } from '@/compilerSfc'
-import { INIT_CODE } from '@/data'
 import { watchRoot } from '@/utils'
 
 export async function awaitCompileSfc(handler: Fn<[SFCStyleCompileResults[], SFCScriptBlock, SFCTemplateCompileResults]>) {
   const clientCodeList = await watchRoot((node, clientCodeList) => {
-    if (/^(?:script|template|style)$/.test(node.localName)) {
-      clientCodeList.push(node.outerHTML)
-      node.localName === 'style'
-        ? (node.onload = node.remove)
-        : node.remove()
-    }
+    if (!/^(?:script|template|style)$/.test(node.localName))
+      return
+    if (node.hasAttribute('src'))
+      return
+    clientCodeList.push(node.outerHTML)
+    node.localName === 'style'
+      ? (node.onload = node.remove)
+      : node.remove()
   })
-  const { sfcScriptBlock, sfcTemplateCompileResults, sfcStyleCompileResultsList } = compilerSfc(/* INIT_CODE + */ clientCodeList.join('\n'))
+  const { sfcScriptBlock, sfcTemplateCompileResults, sfcStyleCompileResultsList } = compilerSfc(clientCodeList.join('\n'))
   handler(sfcStyleCompileResultsList, sfcScriptBlock, sfcTemplateCompileResults)
 }
