32c5f1aedffc5bd28f050a950edad3b284bbc6db
Tofu-Xx <1714843907@qq.com>
2024-11-16 03:40:31 +0800

    chore: sfc2esm0.0.9

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ASCII_LOGO, IMPORTS_TAG_CODE, REPO_NAME } from '@/data'
 import { awaitClientCode } from '@/funcs'
 import { createDom } from '@/utils'
-import { createConvertor } from "sfc2esm";
+import { useXxx2x } from 'sfc2esm'
 
 awaitClientCode((sfcCode) => {
   const site = document[__IS_DEV__ ? 'head' : 'body']
-  const { sfc2esm, sfc2css } = createConvertor({ id: REPO_NAME, appName: 'APP$' })
+  const { esm, css } = useXxx2x({ id: REPO_NAME, appName: 'APP$' }).sfc2x(sfcCode, 'body')
   createDom(IMPORTS_TAG_CODE)?.mount(site)
-  createDom(`<script ${REPO_NAME} type="module">`, ASCII_LOGO + sfc2esm(sfcCode,'body'))?.mount(site)
-  createDom(sfc2css(sfcCode) && `<style ${REPO_NAME}>`, sfc2css(sfcCode))?.mount(document.head)
+  createDom(`<script ${REPO_NAME} type="module">`, ASCII_LOGO + esm)?.mount(site)
+  createDom(css && `<style ${REPO_NAME}>`, css)?.mount(document.head)
 })
