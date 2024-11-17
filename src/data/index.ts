d3c0b517eadb02b15ec0095ba55e7159acc74d62
Tofu-Xx <1714843907@qq.com>
2024-11-17 17:27:58 +0800

    refactor: no data

--- src/data/index.ts
+++
@@ -1,6 +0,0 @@
-export const REPO_NAME = 'setupin'
-export const IMPORTS_TAG_CODE = `
-<script ${REPO_NAME} type="importmap">
-  ${JSON.stringify({ imports: { vue: __VUE_URL__ } })}
-</script>
-` as const
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import asciiLogo from '@/assets/ascii-logo.txt?raw'
 import { useXxx2x } from 'sfc2esm'
 import { awaitClientCode } from './awaitClientCode'
-import { IMPORTS_TAG_CODE, REPO_NAME } from './data'
 import { createDom } from './utils'
 
 awaitClientCode((sfcCode) => {
   const site = document[__IS_DEV__ ? 'head' : 'body']
-  const { esm, css } = useXxx2x({ id: REPO_NAME, appName: 'APP$' }).sfc2x(sfcCode, 'body')
-  createDom(IMPORTS_TAG_CODE)?.mount(site)
-  createDom(`<script ${REPO_NAME} type="module">`, asciiLogo + esm)?.mount(site)
-  createDom(css && `<style ${REPO_NAME}>`, css)?.mount(document.head)
+  const { esm, css } = useXxx2x({ id: 'setupin', appName: 'APP$' }).sfc2x(sfcCode, 'body')
+  createDom('<script type="importmap">', JSON.stringify({ imports: { vue: __VUE_URL__ } }))?.mount(site)
+  createDom('<script type="module">', asciiLogo + esm)?.mount(site)
+  createDom(css && '<style>', css)?.mount(document.head)
 })
